cc92e236f27646d83cf52b833879508888750cff
hany0ageeb <hany0ageeb@gmail.com>
2023-10-08 11:48:31 -0700

    bool.h now can bypass betty

--- bool.h
+++ bool.h
@@ -1,8 +1,13 @@
 #ifndef BOOL_H_
 #define BOOL_H_
-enum boolean
+/**
+ * enum boolean - boolean (TRUE, FALSE)
+ * @FALSE: 0
+ * @TRUE: 1
+ */
+typedef enum boolean
 {
-    FALSE = 0,
-    TRUE = 1
-};
+	FALSE = 0,
+	TRUE = 1
+} bool_t;
 #endif
