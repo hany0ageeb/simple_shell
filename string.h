3e965930d994735390d7874c6d6af4f46cdad4d5
hany0ageeb <hany0ageeb@gmail.com>
2023-10-22 07:46:16 -0700

    oops forget string.h

--- string.h
+++ string.h
@@ -9,6 +9,7 @@ size_t str_len(const char *str);
 char *copy_str(const char *str);
 char *sub_str(const char *str, size_t lo, size_t hi);
 char *concat_str(const char *str1, const char *str2);
+char *concat_strs(size_t count, ...);
 int str_cmp(const char *str1, const char *str2);
 bool_t start_with(const char *str, const char *start);
 size_t count_str_list_len(const char *str, const char separator);
